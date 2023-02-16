e049c62c78b4f705de1998a8e1bf9299690c31e1
& C:/Users/Elchin/AppData/Local/Programs/Python/Python310/python.exe c:/Users/Elchin/OneDrive/Documents/Codes/Python/json-thu1200/script.py <hello@datorium.eu>
2023-02-16 18:05:12 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace CromiumBrowserWinFormsDotNet4_2022_2023
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.chromiumWebBrowser1 = new CefSharp.WinForms.ChromiumWebBrowser();
+            this.Browser = new CefSharp.WinForms.ChromiumWebBrowser();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.Go = new System.Windows.Forms.ToolStripButton();
+            this.Address = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // chromiumWebBrowser1
+            // 
+            this.chromiumWebBrowser1.ActivateBrowserOnCreation = false;
+            this.chromiumWebBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.chromiumWebBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.chromiumWebBrowser1.Name = "chromiumWebBrowser1";
+            this.chromiumWebBrowser1.Size = new System.Drawing.Size(1191, 593);
+            this.chromiumWebBrowser1.TabIndex = 0;
+            // 
+            // Browser
+            // 
+            this.Browser.ActivateBrowserOnCreation = false;
+            this.Browser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Browser.Location = new System.Drawing.Point(0, 0);
+            this.Browser.Name = "Browser";
+            this.Browser.Size = new System.Drawing.Size(1191, 593);
+            this.Browser.TabIndex = 1;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Go,
+            this.Address});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1191, 50);
+            this.toolStrip1.TabIndex = 2;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // Go
+            // 
+            this.Go.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.Go.Image = ((System.Drawing.Image)(resources.GetObject("Go.Image")));
+            this.Go.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.Go.Name = "Go";
+            this.Go.Size = new System.Drawing.Size(28, 47);
+            this.Go.Text = "toolStripButton1";
+            this.Go.Click += new System.EventHandler(this.Go_Click);
+            // 
+            // Address
+            // 
+            this.Address.Font = new System.Drawing.Font("Segoe UI", 24F);
+            this.Address.Name = "Address";
+            this.Address.Size = new System.Drawing.Size(119, 33);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1191, 593);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.Browser);
+            this.Controls.Add(this.chromiumWebBrowser1);
+            this.Name = "Form1";
+            this.Text = "Elchin\'s browser";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private CefSharp.WinForms.ChromiumWebBrowser chromiumWebBrowser1;
+        private CefSharp.WinForms.ChromiumWebBrowser Browser;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton Go;
+        private System.Windows.Forms.ToolStripTextBox Address;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,36 @@
+﻿using CefSharp.WinForms;
+using CefSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CromiumBrowserWinFormsDotNet4_2022_2023
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeBrowswer();
+        }
+
+        public void InitializeBrowswer()
+        {
+            var settings = new CefSettings();
+            Cef.Initialize(settings);
+            Browser.Load("https://google.com");
+        }
+
+        private void Go_Click(object sender, EventArgs e)
+        {
+            Browser.Load(Address.Text);
+        }
+    }
+}
